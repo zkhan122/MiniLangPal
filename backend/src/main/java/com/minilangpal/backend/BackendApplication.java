dcaa1ea624f8040dcfa8dde7f58a5c6bb2d15e66
Zayaan <zayaankashif@yahoo.com>
2025-02-20 20:40:55 +0000

    stash

--- backend/src/main/java/com/minilangpal/backend/BackendApplication.java
+++ backend/src/main/java/com/minilangpal/backend/BackendApplication.java
@@ -1,9 +1,15 @@
 package com.minilangpal.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+// @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class BackendApplication{
 
 	public static void main(String[] args) {
