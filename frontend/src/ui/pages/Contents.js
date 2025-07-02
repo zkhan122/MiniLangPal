39ad80270f70a0b8e3d8b60106f232f00bb69429
Zayaan <zayaankashif@yahoo.com>
2025-07-02 23:47:57 +0100

    push contents

--- frontend/src/ui/pages/Contents.js
+++ frontend/src/ui/pages/Contents.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Alert from '@mui/material/Alert';
 import axios from "axios";
-import '../css/login.css';
+import "../css/quiz-styling.css";   
 
 export default function Contents() {
 
@@ -10,67 +10,52 @@ export default function Contents() {
         
 
     <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Product name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Color
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Category
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="bg-white dark:bg-gray-800">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
-                    </th>
-                    <td class="px-6 py-4">
-                        Silver
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop
-                    </td>
-                    <td class="px-6 py-4">
-                        $2999
-                    </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Microsoft Surface Pro
-                    </th>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop PC
-                    </td>
-                    <td class="px-6 py-4">
-                        $1999
-                    </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Magic Mouse 2
-                    </th>
-                    <td class="px-6 py-4">
-                        Black
-                    </td>
-                    <td class="px-6 py-4">
-                        Accessories
-                    </td>
-                    <td class="px-6 py-4">
-                        $99
-                    </td>
-                </tr>
-            </tbody>
+        <table class="table-auto border-separate border-spacing-x-6 border-spacing-y-3 w-full">
+        <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
+            <tr>
+            <th scope="col" class="px-4 py-2">Lesson</th>
+            <th scope="col" class="px-4 py-2">Level</th>
+            <th scope="col" class="px-4 py-2">Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Greetings & Introductions</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">How to greet someone and introduce yourself</td>
+            </tr>
+                        <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Describing Background</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">How to describe your background such as your country, nationality</td>
+            </tr>
+                        <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">"This is" & "That is"</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">Describing something or someone</td>
+            </tr>
+                        <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Sentences With Pronouns</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">Using pronouns</td>
+            </tr>
+            <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Sentences With Verbs</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">Different verbs and using them in sentences</td>
+            </tr>
+            <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Family</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">Describing your family</td>
+            </tr>
+            <tr>
+                <td class="px-4 py-2"><button type="button" class="btn btn-info">Asking questions</button></td>
+                <td class="px-4 py-2">Beginner</td>
+                <td class="px-4 py-2">Questions you could ask someone based on scenarios</td>
+            </tr>
+
+        </tbody>
         </table>
     </div>
 
