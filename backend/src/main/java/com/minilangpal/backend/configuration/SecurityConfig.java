66609ecf67aeed718eac6057f25d6ef89fc8c8f0
Zayaan <zayaankashif@yahoo.com>
2025-09-10 17:54:36 +0100

    push multi-container dockerization config

--- backend/src/main/java/com/minilangpal/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/minilangpal/backend/configuration/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost"));
         config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
